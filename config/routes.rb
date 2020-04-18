32ca65d6306c367d725423a283b0d89d207a0774
Mayo <memberwws@gmail.com>
2020-04-18 11:57:35 +0900

    ログインページとホームコンテンツを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/contact'
+  get '/home', to:'static_pages#home'
+  get '/contact', to:'static_pages#contact'
+  get '/signup', to:'users#new'
   resources :microposts
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
