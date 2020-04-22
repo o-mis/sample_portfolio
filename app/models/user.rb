2c9b7ac8e1f216bc48681f61055f63246dc4040d
Mayo <memberwws@gmail.com>
2020-04-22 09:57:02 +0900

    ログイン画面のレイアウトを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :omniauthable, omniauth_providers: [:twitter]
   has_many :microposts, dependent: :destroy
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }
+  has_secure_password
+  validates :password, presence: true, length: { maximum: 6 }
 end
