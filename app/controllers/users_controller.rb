14ea65f1417b6dfa6e1424e1d2849aaa0b61e1f9
Mayo <memberwws@gmail.com>
2020-04-27 17:37:18 +0900

    ナビバーにブックマークを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,9 @@ class UsersController < ApplicationController
   end
 
   # GET /users/1/edit
-  def edit; end
+  def edit
+    @user = User.find(params[:id])
+  end
 
   # POST /users
   # POST /users.json
