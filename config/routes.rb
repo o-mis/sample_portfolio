6593287cd5b1cf7022834c1a9b04d5d0345094d4
Mayo <memberwws@gmail.com>
2020-05-05 16:28:31 +0900

    ログイン画面のレイアウト修正とリファクタリング

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'omniauth_callbacks',
+    sessions: 'users/sessions'
+  }
+
+  devise_scope :user do
+    get 'sign_in', to: 'users/sessions#new'
+    get 'sign_out', to: "controller#method" 'sign_out', to: 'users/sessions#destroy'
+  end
+
   root 'static_pages#home'
   get '/home', to:'static_pages#home'
-  get '/contact', to:'static_pages#contact'
-  get '/signup', to:'users#new'
+  #get '/contact', to:'static_pages#contact'
+  #get '/signup', to:'users#new'
   resources :microposts
   resources :users
 end
