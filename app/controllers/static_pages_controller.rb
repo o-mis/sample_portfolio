b1ed58029a6b15f764a52e57d63f2ff66754e40b
Mayo <memberwws@gmail.com>
2020-05-30 00:57:06 +0900

    フィードのレイアウト修正、ラジオボタン改良

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,13 @@
 class StaticPagesController < ApplicationController
+
   def home
     @micropost = current_user.microposts.create
+    @microposts = Micropost.page(params[:page]).per(6)
   end
+
+  # def index
+  #   @microposts = Micropost.page(params[:page]).per(6)
+  # end
 
   # private
   # def micropost_params
