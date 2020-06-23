11c2e5e9c15d394ee86e4020a539af45ba90f085
Mayo <memberwws@gmail.com>
2020-06-24 04:42:59 +0900

    レイアウト修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
-    @micropost = current_user.microposts.create
-    @microposts = Micropost.page(params[:page]).per(6)
+    # @micropost = current_user.microposts.page(params[:page]).per(15)
+    # @microposts = Micropost.page(params[:page]).per(15)
+    @feed = current_user.feed.page(params[:page]).per(15)
   end
 
   # private
--- app/models/user.rb
+++ app/models/user.rb
@@ -40,4 +40,12 @@ class User < ApplicationRecord
   def following?(other_user)
     following.include?(other_user)
   end
+
+  # 自分とフォローしているユーザーの投稿を取得
+  def feed
+    following_ids = "SELECT followed_id FROM relationships
+                    WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                    OR user_id = :user_id", user_id: id)
+  end
 end
