88c2653766b9344aa2ebecd0ebff60504f8f43b5
Mayo <memberwws@gmail.com>
2020-07-14 20:14:39 +0900

    投稿フォームに未選択のラジオボタンを追加

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -27,10 +27,10 @@ class Micropost < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   mount_uploader :image, ImageUploader
 
-  enum arrived_at: { 昼: 0, 夜: 1 }
+  enum arrived_at: { 未選択: 0, 昼: 1, 夜: 2 }
 
-  geocoded_by :address
-  before_validation :geocode
+  # geocoded_by :address
+  # before_validation :geocode
 
   def like(user)
     likes.create(user_id: user.id)
