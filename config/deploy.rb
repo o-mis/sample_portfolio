c4e829f923bbfba93776f9478c127035b8e416a9
Mayo <memberwws@gmail.com>
2020-08-04 18:26:29 +0900

    fix: capistranoの設定を修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :deploy_to, "/var/www/rails/Contrail"
 
 set :pty, true
 
-append :linked_files, "config/credentials/master.key"
+append :linked_files, "config/master.key"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor/bundle"
 
 set :keep_releases, 5
