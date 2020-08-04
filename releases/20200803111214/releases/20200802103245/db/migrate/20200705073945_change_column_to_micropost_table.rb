5e50a7d2c9cb855ba29a7fc537dd29d9b63bb18c
Mayo <memberwws@gmail.com>
2020-08-04 18:59:28 +0900

    fix: capistranoの設定を修正

--- releases/20200803111214/releases/20200802103245/db/migrate/20200705073945_change_column_to_micropost_table.rb
+++
@@ -1,9 +0,0 @@
-class ChangeColumnToMicropostTable < ActiveRecord::Migration[6.0]
-  def up
-    change_column_null :microposts, :content, false
-  end
-
-  def down
-    change_column_null :microposts, :content, true
-  end
-end
