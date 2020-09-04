57440c1d92cc094c8ffa4fade633d318ae88f88d
Mayo <memberwws@gmail.com>
2020-09-04 14:41:20 +0900

    [fix] gitignoreとディレクトリ構成を修正

--- releases/20200807123015/db/migrate/20200719103641_remove_restaurant_from_microposts.rb
+++
@@ -1,5 +0,0 @@
-class RemoveRestaurantFromMicroposts < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :microposts, :restaurant, :text
-  end
-end
--- releases/20200807123309/db/migrate/20200520140801_add_columns_to_microposts.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToMicroposts < ActiveRecord::Migration[6.0]
-  def change
-    add_column :microposts, :budget, :decimal, precision: 5, scale: 3
-    add_column :microposts, :restaurant, :text
-  end
-end
