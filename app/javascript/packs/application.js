0654b7489dea34af43d1bea119c0b1b309cb16f1
Mayo <memberwws@gmail.com>
2020-09-23 01:57:02 +0900

    [fix] 非ログイン画面に新着投稿を表示

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,8 +6,13 @@
 require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
-//= require("channels")
-//= require('jquery');
+// = require("channels")
+// = require('jquery');
+// = require infinite-scroll.pkgd.min.js
+// = require infinite-scroll/dist/infinite-scroll.pkgd.min.js
+// = require_tree .
+
+// import "../js/microposts";
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
