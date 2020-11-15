8515a53c1c3e0916f2f87d40ae65b34dbba4a1a6
Mayo <memberwws@gmail.com>
2020-11-15 17:22:31 +0900

    [add] フォロー関係のSystem Specを追加

--- spec/system/relationships_spec.rb
+++ spec/system/relationships_spec.rb
@@ -4,14 +4,16 @@ RSpec.describe 'Relationships', type: :system, js: true do
   describe 'フォロー,フォロー解除' do
     it 'ログインユーザーがフォロー/フォロー解除できること' do
       bob = create(:user,
+                   name: 'Bob',
                    email: 'bob@example.com',
                    password: 'password_bob')
 
       alice = create(:user,
+                     name: 'Alice',
                      email: 'alice@example.com',
                      password: 'password_alice')
 
-      # bobのログイン
+      # bobがログイン
       visit '/sign_in'
       fill_in 'メールアドレス', with: bob.email
       fill_in 'パスワード', with: bob.password
@@ -23,10 +25,28 @@ RSpec.describe 'Relationships', type: :system, js: true do
       expect(page).to have_content '0 フォロワー'
 
       expect do
-        click_button 'フォローする'
-        expect(page).to have_content 'フォロー中'
-      end.to change(bob.following, :count).by(1)
+        find('.follow-btn').click
+        expect(page).to have_content '1 フォロワー'
+      end.to change(bob.following, :count).by(1) &
+             change(alice.followers, :count).by(1)
+
+      # bobがマイページから確認
+      visit user_path(bob)
+      expect(page).to have_content '1 フォロー中'
+
+      # aliceをユーザーページからフォロー解除
+      visit user_path(alice)
       expect(page).to have_content '1 フォロワー'
+
+      expect do
+        find('.unfollow-btn').click
+        expect(page).to have_content '0 フォロワー'
+      end.to change(bob.following, :count).by(-1) &
+             change(alice.followers, :count).by(-1)
+
+      # bobがマイページから確認
+      visit user_path(bob)
+      expect(page).to have_content '0 フォロー中'
     end
   end
 end
