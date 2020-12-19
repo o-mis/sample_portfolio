71eca7550ac234e658e5c62192333f3602a4b105
Mayo <memberwws@gmail.com>
2020-12-19 13:07:05 +0900

    再設定

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,10 +1,11 @@
 Rails.application.config.session_store :redis_store, {
   servers: [
     {
-      host: 'ec-redis-0001-001.besjzl.0001.apne1.cache.amazonaws.com',
+      host: 'ec-redis.besjzl.clustercfg.apne1.cache.amazonaws.com',
       port: 6379,
       db: 0,
-      namespace: 'session' # 名前空間。"session:セッションID"の形式
+      namespace: 'session', # 名前空間。"session:セッションID"の形式
+      timeout: 240
     }
   ],
   expire_after: 90.minutes
